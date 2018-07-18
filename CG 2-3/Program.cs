8612023b0a3fbfbc12e39797c3625be7acc414ca
Kristen Shea <kstar61@live.com>
2018-07-17 21:10:15 -0500

    Add project files.

---
+++ CG 2-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CG_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("When were you born?");
+            Console.ReadLine();
+
+            Console.Write("Please type your full month of birth (ex. September): ");
+            String birthMonth;
+            birthMonth = Console.ReadLine();
+
+            Console.Write("Day of birth: ");
+            String birthDay;
+            birthDay = Console.ReadLine();
+            int birthDayInt = int.Parse(birthDay);
+            if (birthDay.Length >2) return;
+            
+            Console.Write("Year of birth: ");
+            String birthYear;
+            birthYear = Console.ReadLine();
+            int birthYearInt = int.Parse(birthYear);
+            if (birthYear.Length >4 || birthYear.Length <4) return;
+            
+            Console.WriteLine("My date of birth is " + birthMonth + " " + birthDayInt + "," + " " + birthYearInt + ".");
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
